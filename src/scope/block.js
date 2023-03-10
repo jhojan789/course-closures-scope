7a3af219c9f0b1eba8ffb441ded27d349424ee19
Jhojan Tobón <jhojantobonm@gmail.com>
2023-03-10 13:03:45 -0500

    block.js created

---
+++ src/scope/block.js
@@ -0,0 +1,14 @@
+//block scope
+
+function fruits(){
+    if(true){
+        var fruit1 = "Apple";//function scope
+        let fruit2 = "Kiwi";//block scope
+        const fruit3 = "Banana";//block scope
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+
+}
+fruits();
