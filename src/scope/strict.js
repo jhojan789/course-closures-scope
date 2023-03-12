4ccac28e0380b34e6c2227f76d469cb5a2c58fb4
Jhojan Tobón <jhojantobonm@gmail.com>
2023-03-12 12:07:59 -0500

    strict.js created

---
+++ src/scope/strict.js
@@ -0,0 +1,14 @@
+'use strict' //it doesn't allow the hoisting
+pi = 3.1416;
+// console.log(pi);
+
+const a = 'g';
+console.log(a);
+
+function myFunction(){
+    b='b';
+    console.log(b);
+
+}
+
+myFunction();
